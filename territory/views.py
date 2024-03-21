e5910b73f15c2474e98f789b9b2902505665a39e
saradonin <m.jemieljanczuk@gmail.com>
2024-03-21 22:02:32 +0100

    list view added

--- territory/views.py
+++ territory/views.py
@@ -1,3 +1,12 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from django.shortcuts import render
+from territory.models import TerritorialUnits
+from territory.serializers import TerritorialUnitsSerializer
 
-# Create your views here.
+
+class TerritorialUnitsListView(generics.ListAPIView):
+    queryset = TerritorialUnits.objects.all()
+    serializer_class = TerritorialUnitsSerializer
+    permission_classes = (AllowAny, )
+    
